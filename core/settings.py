4d463c451adbfad062e5dac21593618a9e4c2f03
Queror254 <mwendavictorm@gmail.com>
2025-03-30 17:00:52 +0300

    upt allowed_host

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v5lal&ljhar3u0$%@6jt=p2qy^rir$s+!hlb&fyckc3ukqv&ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
