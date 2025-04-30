55c0803cad86c3f98e6336797d7dd7a827a366b0
Queror254 <mwendavictorm@gmail.com>
2025-05-01 02:16:07 +0300

    implemented the business settings

--- setting_app/models.py
+++ setting_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from users.models import Business
 
 class Rates(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True, blank=True)
     weight_rate = models.FloatField(verbose_name="Rate per Kilogram")
     cbm_rate = models.FloatField(verbose_name="Rate per Cubic Meter")
 
--- setting_app/urls.py
+++ setting_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import business_settings, update_weight_rate, update_cbm_rate
-
+from users.views import update_admin_profile
 urlpatterns = [
     path("rates/", business_settings, name="business_settings"),
+    path('business/', update_admin_profile, name="general_settings"),
     path("update-weight/", update_weight_rate, name="update_weight_rate"),
     path("update-cbm/", update_cbm_rate, name="update_cbm_rate"),
 ]
